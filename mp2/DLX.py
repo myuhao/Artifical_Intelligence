82e18b80d7f388431b2c9f193679dbf63e37ce33
Yuhao Min <myuhao@gmail.com>
2019-02-18 02:34:16 -0600

    Implemented _cover() method

--- mp2/DLX.py
+++ mp2/DLX.py
@@ -1,11 +1,12 @@
 import numpy as np;
+import sys;
 
 
 class DLX:
 
     class Node:
         def __init__(self, left=None, right=None, up=None, down=None):
-            self.l = left # Type = Node.
+            self.l = left # Type = Node, left node.
             self.r = right
             self.u = up
             self.d = down
@@ -15,15 +16,14 @@ class DLX:
             self.nodeCt = 0 # Type = int, Number of nodes in the column.
 
         def print(self):
-            print("Node rowID is {}, colID is {}".format(self.rowID, self.colID))
-            print()
+            print("Node rowID and colID is ({}, {}).".format(self.rowID, self.colID))
 
     def __init__(self, A):
         '''
         Ctor. A is the input np matrix. The first row is pedded with np.ones((nCol,))
         '''
         self.header = self._convertMatToHeader(A)
-
+        self.shape = A.shape
 
     def _convertMatToHeader(self, A):
         """
@@ -64,30 +64,30 @@ class DLX:
                     matrix[i][j].rowID = i
                     matrix[i][j].colID = j
 
-                    # Point the node to its neighbors.
+                    # Get the next 1 and have current node point to it.
                     # --------------------------------------------------
-                    # Right
+                    # Right neighbor.
                     currColIndex = j
                     while True:
                         currColIndex = moveRight(currColIndex)
                         if currColIndex == j or A[i][currColIndex] == 1:
                             break
                     matrix[i][j].r = matrix[i][currColIndex]
-                    # Left
+                    # Left neighbor.
                     currColIndex = j
                     while True:
                         currColIndex = moveLeft(currColIndex)
                         if currColIndex == j or A[i][currColIndex] == 1:
                             break
                     matrix[i][j].l = matrix[i][currColIndex]
-                    # Down
+                    # Down neighbor.
                     currRowIndex = i
                     while True:
                         currRowIndex = moveDown(currRowIndex)
                         if currRowIndex == i or A[currRowIndex][j] == 1:
                             break
                     matrix[i][j].d = matrix[currRowIndex][j]
-                    # Up
+                    # Up neighbor.
                     currRowIndex = i
                     while True:
                         currRowIndex = moveUp(currRowIndex)
@@ -98,19 +98,81 @@ class DLX:
                     # print("({},{}) is linked to ({}, {})".format(i,j,i,currColIndex))
         # Connect the header node.
         header = self.Node()
+        header.nodeCt = sys.maxsize
         header.r = matrix[0][0]
         header.l = matrix[0][nCol-1]
         matrix[0][0].l = header
         matrix[0][nCol-1].r = header
         return header
 
-    def print(self):
-        # print(self.header.r.nodeCt)
-        curr = self.header
-        for i in range(10):
-            curr.print()
-            curr = curr.r
+    def _getMinColHeaderNode(self):
+        '''
+        Get the column header of the column with the smallest number
+        of node.
+        If nodeCt is the same, use the column with lower column index.
+        Returns:
+            Node -- The column header that has the lowest nodeCt.
+        Raises:
+            IndexError -- The matrix is empty.
+        '''
+        currNode = self.header.r
+        minNode = currNode
+        if currNode == None:
+            print("ERROR: EMPTY MATRIX.")
+            raise IndexError
+        while currNode != self.header:
+            minNode = minNode if minNode.nodeCt<=currNode.nodeCt else currNode
+            currNode = currNode.r
+        return minNode
 
+    def _cover(self, nNode):
+        # Remove the column header first.
+        colHeader = nNode.column
+        colHeader.r.l = colHeader.l
+        colHeader.l.r = colHeader.r
+
+        # Move down the linked list and remove rows.
+        # Initialized at the first node that is not the column header.
+        currRow = colHeader.d
+        # We will move down this column
+        while currRow != nNode.column:
+            # When encountered a node, we will move right.
+            currCol = currRow.r
+            while currCol != currRow:
+                # When encountered a node, detached it from the column.
+                currCol.d.u = currCol.u
+                currCol.u.d = currCol.d
+                # Decreament the nodeCt.
+                currCol.column.nodeCt -= 1
+
+            # Increament the currNode.
+                currCol = currCol.r
+            currRow = currRow.d
+
+    def _uncover(self, nNode):
+        pass
+
+    def print(self):
+        currNode = self.header.r
+        while currNode != self.header:
+            currNode.print()
+            print(currNode.nodeCt)
+            currNode.d.print()
+            print()
+            currNode = currNode.r
+
+    def convertToMatrix(self):
+        mat = np.zeros(self.shape)
+        mat[0,:] = np.ones((self.shape[1],))
+        currColHeader = self.header.r
+        for i in range(self.shape[1]-1):
+            currNode = currColHeader.d
+            while currNode != currColHeader:
+                currNode.print()
+                mat[currNode.rowID, currNode.colID] = 1
+                currNode = currNode.d
+            currColHeader = currColHeader.r
+        return mat
 
 def test():
     rowHeader = np.ones((7,))
@@ -122,11 +184,22 @@ def test():
     row4 = np.array([0, 1, 1, 0, 0, 1, 1])
     row5 = np.array([0, 1, 0, 0, 0, 0, 1])
     row6 = np.array([1, 0, 0, 1, 0, 0, 0])
-
     MAT = np.array([rowHeader, row0, row1, row2, row3, row4, row5, row6]) # Ans = [6, 4, 2] [6, 4, 7] (1-indexed)
+
+    rowHeader = np.ones((7,))
+    # rowHeader =   [1, 1, 1, 1, 1, 1, 1]
+    row0 = np.array([0, 0, 1, 0, 1, 1, 0])
+    row1 = np.array([1, 0, 0, 1, 0, 0, 1])
+    row2 = np.array([0, 1, 1, 0, 0, 1, 0])
+    row3 = np.array([1, 0, 0, 1, 0, 0, 0])
+    row4 = np.array([0, 1, 0, 0, 0, 0, 1])
+    row5 = np.array([0, 0, 0, 1, 1, 0, 1])
+    MAT = np.array([rowHeader, row0, row1, row2, row3, row4, row5]) # Ans = [6, 4, 2] [6, 4, 7] (1-indexed)
 
 
     solution = DLX(MAT)
+    solution._cover(solution.header.r.d)
+    # print(solution.convertToMatrix())
     solution.print()
 
 
