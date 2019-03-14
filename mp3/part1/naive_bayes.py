cbc39649323919e3717867dca5c80a629b65a6c6
Yuhao Min <myuhao@gmail.com>
2019-03-14 16:41:19 -0500

    initial commit

--- mp3/part1/naive_bayes.py
+++ mp3/part1/naive_bayes.py
@@ -2,19 +2,17 @@ import numpy as np
 
 class NaiveBayes(object):
 	def __init__(self,num_class,feature_dim,num_value):
-		"""Initialize a naive bayes model. 
-
-		This function will initialize prior and likelihood, where 
+		"""Initialize a naive bayes model.
+		This function will initialize prior and likelihood, where
 		prior is P(class) with a dimension of (# of class,)
 			that estimates the empirical frequencies of different classes in the training set.
-		likelihood is P(F_i = f | class) with a dimension of 
+		likelihood is P(F_i = f | class) with a dimension of
 			(# of features/pixels per image, # of possible values per pixel, # of class),
-			that computes the probability of every pixel location i being value f for every class label.  
-
+			that computes the probability of every pixel location i being value f for every class label.
 		Args:
 		    num_class(int): number of classes to classify
-		    feature_dim(int): feature dimension for each example 
-		    num_value(int): number of possible values for each pixel 
+		    feature_dim(int): feature dimension for each example
+		    num_value(int): number of possible values for each pixel
 		"""
 
 		self.num_value = num_value
@@ -25,12 +23,11 @@ class NaiveBayes(object):
 		self.likelihood = np.zeros((feature_dim,num_value,num_class))
 
 	def train(self,train_set,train_label):
-		""" Train naive bayes model (self.prior and self.likelihood) with training dataset. 
+		""" Train naive bayes model (self.prior and self.likelihood) with training dataset.
 			self.prior(numpy.ndarray): training set class prior (in log) with a dimension of (# of class,),
-			self.likelihood(numpy.ndarray): traing set likelihood (in log) with a dimension of 
+			self.likelihood(numpy.ndarray): traing set likelihood (in log) with a dimension of
 				(# of features/pixels per image, # of possible values per pixel, # of class).
-			You should apply Laplace smoothing to compute the likelihood. 
-
+			You should apply Laplace smoothing to compute the likelihood.
 		Args:
 		    train_set(numpy.ndarray): training examples with a dimension of (# of examples, feature_dim)
 		    train_label(numpy.ndarray): training labels with a dimension of (# of examples, )
@@ -41,18 +38,16 @@ class NaiveBayes(object):
 
 	def test(self,test_set,test_label):
 		""" Test the trained naive bayes model (self.prior and self.likelihood) on testing dataset,
-			by performing maximum a posteriori (MAP) classification.  
-			The accuracy is computed as the average of correctness 
-			by comparing between predicted label and true label. 
-
+			by performing maximum a posteriori (MAP) classification.
+			The accuracy is computed as the average of correctness
+			by comparing between predicted label and true label.
 		Args:
 		    test_set(numpy.ndarray): testing examples with a dimension of (# of examples, feature_dim)
 		    test_label(numpy.ndarray): testing labels with a dimension of (# of examples, )
-
 		Returns:
-			accuracy(float): average accuracy value  
+			accuracy(float): average accuracy value
 			pred_label(numpy.ndarray): predicted labels with a dimension of (# of examples, )
-		"""    
+		"""
 
 		# YOUR CODE HERE
 
@@ -65,15 +60,15 @@ class NaiveBayes(object):
 
 
 	def save_model(self, prior, likelihood):
-		""" Save the trained model parameters 
-		"""    
+		""" Save the trained model parameters
+		"""
 
 		np.save(prior, self.prior)
 		np.save(likelihood, self.likelihood)
 
 	def load_model(self, prior, likelihood):
-		""" Load the trained model parameters 
-		""" 
+		""" Load the trained model parameters
+		"""
 
 		self.prior = np.load(prior)
 		self.likelihood = np.load(likelihood)
@@ -83,8 +78,8 @@ class NaiveBayes(object):
 	    Get the feature likelihoods for high intensity pixels for each of the classes,
 	        by sum the probabilities of the top 128 intensities at each pixel location,
 	        sum k<-128:255 P(F_i = k | c).
-	        This helps generate visualization of trained likelihood images. 
-	    
+	        This helps generate visualization of trained likelihood images.
+
 	    Args:
 	        likelihood(numpy.ndarray): likelihood (in log) with a dimension of
 	            (# of features/pixels per image, # of possible values per pixel, # of class)
@@ -93,7 +88,7 @@ class NaiveBayes(object):
 	            (# of features/pixels per image, # of class)
 	    """
 	    # YOUR CODE HERE
-	    
+
 	    feature_likelihoods = np.zeros((likelihood.shape[0],likelihood.shape[2]))
 
 	    return feature_likelihoods
