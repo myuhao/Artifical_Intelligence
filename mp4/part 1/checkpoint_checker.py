1321bf5220519e566f20d7d05f17371b62010594
Yuhao Min <myuhao@gmail.com>
2019-04-18 00:06:05 -0500

    Add TA provided checking script.

--- mp4/part 1/checkpoint_checker.py
+++ mp4/part 1/checkpoint_checker.py
@@ -15,35 +15,33 @@ def compare_checkpoints(number=1):
     differences = my_checkpoint == ta_checkpoint
     are_same = np.all(differences)
 
-    print ("The checkpoints contain the same values: " + str(are_same))
-
+    count = 0
     if not are_same:
-
         # find the locations where the arrays differ
         indx_diff = np.where(differences == False)
-        print ("\nDifferences occur at the following locations in Q-table: ")
         location = []
         for i in range (np.shape(indx_diff)[1]):
             for j in range (np.shape(indx_diff)[0]):
                 location.append(indx_diff[j][i])
-            
-            print ("\nAt : " + str(location) + " , the values are: ")
-            print ("Mine: " + str(my_checkpoint[tuple(location)]))
-            print ("TA's: " + str(ta_checkpoint[tuple(location)]))
+            if np.around(my_checkpoint[tuple(location)], decimals=8) != np.around(ta_checkpoint[tuple(location)], decimals=8):
+                print ("\nAt : " + str(location) + " , the values are: ")
+                print ("Mine: " + str(my_checkpoint[tuple(location)]))
+                print ("TA's: " + str(ta_checkpoint[tuple(location)]))
+                count += 1
             location = []
-
+    if not count:
+        print ("The checkpoints contain the same values.")
 if __name__ == "__main__":
     """
     Usage:  python checkpoint#
-            
+
             checkpoint# should range from 1-3
             if checkpoint# not specified, will run with checkpoint1.npy
     """
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--checkpoint_num', type= int, default= 1, 
-    #         help= 'choosing which checkpoint to compare with - default 1')
-    # arg = parser.parse_args()
-    compare_checkpoints(1)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint_num', type= int, default= 1,
+            help= 'choosing which checkpoint to compare with - default 1')
+    arg = parser.parse_args()
+    compare_checkpoints(arg.checkpoint_num)
 
